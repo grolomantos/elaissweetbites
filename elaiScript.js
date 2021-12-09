38dd9bb1c2344a9539c30bf0ad49b4e13b3d496a
grolomantos <92844508+grolomantos@users.noreply.github.com>
2021-12-09 18:55:55 +0800

    messenger fix 2 done

--- elaiScript.js
+++ elaiScript.js
@@ -76,3 +76,26 @@ function removeFaqClasses() {
     faq.classList.remove('active')
   })
 }
+
+
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "634799127168009");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+
+//Your SDK code
+
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v12.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
